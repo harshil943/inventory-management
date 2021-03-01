a7d423763a78cea55ad7b33d6efe146f14e0c342
Jainam Shah <shahjainamn@gmail.com>
2021-03-01 18:24:58 +0530

    Login Theme using Insipia

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -8,39 +8,9 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    {{-- <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">About Us</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Product Categories
-                          </a>
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                          </div>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Quality</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Brochure</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Contect us</a>
-                        </li>
-                    </ul> --}}
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
                           <a class="nav-link" href="#">
-                            
                             Home
                             <span class="sr-only">(current)</span>
                             </a>
@@ -48,13 +18,11 @@
                         
                         <li class="nav-item">
                           <a class="nav-link" href="#">
-                            
                             About us
                           </a>
                         </li>
                         <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          
                             Product Category
                           </a>
                           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -66,19 +34,16 @@
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">
-                              
                               Quality
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">
-                              
                               Brochure
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">
-                              
                               Contact Us
                             </a>
                         </li>
