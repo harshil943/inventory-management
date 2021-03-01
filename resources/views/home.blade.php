7dd1ee6e89ed1ccb73d21d776fab4fb2691e43c0
Harshil Amreliya <amreliyaharsh@gmail.com>
2021-03-01 16:15:50 +0530

    Home page changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -237,5 +237,30 @@
         </div>
     </nav> --}}
     @include('layouts.nav')
-    <h1 style="color:white;">User Home</h1>
+    <section class="section-main bg-light padding-y mt-5">
+        <div class="container">
+        
+        <div class="row">
+            <aside class="col-md-3 my-auto">
+                <nav class="card">
+                    <div class="card-headder mx-auto f">Most Popular</div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="#">Best clothes</a></li>
+                        <li class="list-group-item"><a href="#">Automobiles</a></li>
+                        <li class="list-group-item"><a href="#">Home interior</a></li>
+                        <li class="list-group-item"><a href="#">Electronics</a></li>
+                        <li class="list-group-item"><a href="#">Technologies</a></li>
+                        <li class="list-group-item"><a href="#">Digital goods</a></li>
+                        
+                    </ul>
+                </nav>
+            </aside> <!-- col.// -->
+            <div class="col-md-9 my-auto">
+                <article class="banner-wrap">
+                    <img src="{{asset('img/hero.jpg')}}" class="w-100 rounded" style="height: 350px">
+                </article>
+            </div> <!-- col.// -->
+        </div> <!-- row.// -->
+        </div> <!-- container //  -->
+        </section>
 @endsection
