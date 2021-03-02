66c1c56e106a9fa55cc1fa3e0e50e940d7566808
Harshil Amreliya <amreliyaharsh@gmail.com>
2021-03-02 17:41:17 +0530

    tables and models created

---
+++ app/Models/MachineErrorReportTable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MachineErrorReportTable extends Model
+{
+    use HasFactory;
+}
