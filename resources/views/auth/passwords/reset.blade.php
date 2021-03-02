4c9303b8739a63b9b486afefe1315bee8be89ac7
Harshil Amreliya <amreliyaharsh@gmail.com>
2021-03-02 10:52:56 +0530

    billing blade and order page modification

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+Hello
+{{-- <div class="passwordBox animated fadeInDown">
+    <div class="row">
+
+        <div class="col-md-12">
+            <div class="ibox-content">
+
+                <h2 class="font-bold">Forgot password</h2>
+
+                <p>
+                    Enter your email address and your password will be reset and emailed to you.
+                </p>
+
+                <div class="row">
+
+                    <div class="col-lg-12">
+                        <form class="m-t" role="form" action="index.html">
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="Email address" required="">
+                            </div>
+
+                            <button type="submit" class="btn btn-primary block full-width m-b">Send new password</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr/>
+    <div class="row">
+        <div class="col-md-6">
+            Copyright Example Company
+        </div>
+        <div class="col-md-6 text-right">
+           <small>© 2014-2015</small>
+        </div>
+    </div>
+</div> --}}
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -61,5 +102,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
