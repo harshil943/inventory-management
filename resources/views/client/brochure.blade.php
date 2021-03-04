b92ef23bcef212373395db7fcbf3207165de5e85
Jainam Shah <shahjainamn@gmail.com>
2021-03-04 12:19:23 +0530

    Brochure Page style Updated

--- resources/views/client/brochure.blade.php
+++ resources/views/client/brochure.blade.php
@@ -4,34 +4,79 @@
     Brochures | Bright Containers
 @endsection
 
+@section('css')
+    <style>
+        .contact-box {
+            margin:10px;
+        }
+    </style>    
+@endsection
+
 @section('content')
 {{-- Temp --}}
-{{$id = 1}}
+@php
+    $id = 1
+@endphp
 {{-- EndTemp --}}
 
-<div class="wrapper wrapper-content animated fadeInRight">
-    <div class="row">
-        <div class="col-lg-3">
-            <div class="contact-box center-version">
-                <a href="">
-                    <img alt="image" class="img-circle" src="img/a2.jpg">
-                    <h3 class="m-b-xs"><strong>John Smith</strong></h3>
-                    <div class="font-bold">Graphics designer</div>
-                    <address class="m-t-md">
-                        <strong>Twitter, Inc.</strong><br>
-                        795 Folsom Ave, Suite 600<br>
-                        San Francisco, CA 94107<br>
-                        <abbr title="Phone">P:</abbr> (123) 456-7890
-                    </address>
-                </a> 
-                <div class="contact-box-footer">
-                    <div class="m-t-xs btn-group">
-                        <a class="btn btn-xs btn-white" href="{{url('brochureDetails',[$id=>'1'])}}"><i class="fa fa-arrow-right"></i> Preview </a>
-                    </div>
-                </div>
+    <div class="container wrapper wrapper-content animated fadeInRight">
+        <div class="row">
 
+            <div class="contact-box center-version col-sm-3">
+                <a href="{{url('brochureDetails',[$id=>'1'])}}" style="text-decoration: none">
+                    <img alt="image" class="img" src="img/a2.jpg">
+                    <div class="contact-box-footer">
+                        <div class="m-t-xs btn-group">
+                            <h3>Brochure Name</h3>
+                        </div>
+                    </div>
+                </a> 
             </div>
+
+            <div class="contact-box center-version col-sm-3">
+                <a href="{{url('brochureDetails',[$id=>'2'])}}" style="text-decoration: none">
+                    <img alt="image" class="img" src="img/a2.jpg">
+                    <div class="contact-box-footer">
+                        <div class="m-t-xs btn-group">
+                            <h3>Brochure Name</h3>
+                        </div>
+                    </div>
+                </a> 
+            </div>
+
+            <div class="contact-box center-version col-sm-3">
+                <a href="{{url('brochureDetails',[$id=>'3'])}}" style="text-decoration: none">
+                    <img alt="image" class="img" src="img/a2.jpg">
+                    <div class="contact-box-footer">
+                        <div class="m-t-xs btn-group">
+                            <h3>Brochure Name</h3>
+                        </div>
+                    </div>
+                </a> 
+            </div>
+
+            <div class="contact-box center-version col-sm-3">
+                <a href="{{url('brochureDetails',[$id=>'4'])}}" style="text-decoration: none">
+                    <img alt="image" class="img" src="img/a2.jpg">
+                    <div class="contact-box-footer">
+                        <div class="m-t-xs btn-group">
+                            <h3>Brochure Name</h3>
+                        </div>
+                    </div>
+                </a> 
+            </div>
+
+            <div class="contact-box center-version col-sm-3">
+                <a href="{{url('brochureDetails',[$id=>'5'])}}" style="text-decoration: none">
+                    <img alt="image" class="img" src="img/a2.jpg">
+                    <div class="contact-box-footer">
+                        <div class="m-t-xs btn-group">
+                            <h3>Brochure Name</h3>
+                        </div>
+                    </div>
+                </a> 
+            </div>
+
         </div>
     </div>
-</div>
-@endsection
+@endsection 
