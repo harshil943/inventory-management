f11776ccaa739c05e4bcea9935f39d2cb163f78a
Jainam Shah <shahjainamn@gmail.com>
2021-03-08 15:09:48 +0530

    Register Final Design And Order Page with roles

--- app/Repositories/orderRepository.php
+++ app/Repositories/orderRepository.php
@@ -12,26 +12,22 @@ class orderRepository implements OrderInterface
 {
     public function all()
     {
-        // $user = Auth::user();
-        // if($user->hasRole('super-admin'))
-        // {
-            
-            // return DB::table('order_details')->get();
+        $user = Auth::user();
+        if($user->hasRole('super-admin') || $user->hasRole('admin'))
+        {
+                return DB::table('order_details')
+                        ->join('product_details', 'order_details.product_id', '=','product_details.id')
+                        ->select('order_details.id','product_details.product_name','order_details.quantity','order_details.order_status','order_details.payment_status')
+                        ->get();
+        }
+        else
+        {
+            $user_id = Auth::user()->id;
             return DB::table('order_details')
                     ->join('product_details', 'order_details.product_id', '=','product_details.id')
                     ->select('order_details.id','product_details.product_name','order_details.quantity','order_details.order_status','order_details.payment_status')
+                    ->where('order_details.user_id',$user_id)
                     ->get();
-            // return OrderDetails::;
-            // select id,product_name,quantity,order_status,payment_status from order_details left join product_details;
-        // }
-        // else
-        // {
-        //     $user_id = Auth::user()->id;
-        //     return DB::table('order_details')->where('user_id',$user_id)->get();
-        // }
-        
-        
+        }
     }
-
-   
 }
