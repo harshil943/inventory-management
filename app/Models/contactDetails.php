7956d8e19afbfb6f994108bcb4d105905cf396e9
Harshil Amreliya <amreliyaharsh@gmail.com>
2021-03-08 17:34:07 +0530

    admin panel changes

--- app/Models/contactDetails.php
+++ app/Models/contactDetails.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class contactDetails extends Model
 {
     use HasFactory;
+
+    protected $table = 'contact_details';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'contact_number',
+        'company_name',
+        'product_category',
+        'product_name',
+        'query',
+    ];
 }
--- app/Models/quoteDetails.php
+++ app/Models/quoteDetails.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class quoteDetails extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $table = 'quote_details';
+
+    protected $fillable = [
+        'company_name',
+        'email',
+        'contact_number',
+        'select_product_size',
+        'quantity_needed',
+    ];
 }
