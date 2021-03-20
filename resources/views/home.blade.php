3d92db819597ea39ada05e4fb02f6ea5cf037f67
jainam4320 <45946941+jainam4320@users.noreply.github.com>
2021-03-20 12:58:40 +0530

    Harshil Push Error Changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,31 +5,109 @@
 @endsection
 
 @section('content')
-    
-    <section class="section-main bg-light padding-y mt-5">
-        <div class="container">
-        
-        <div class="row">
-            <aside class="col-md-3 my-auto">
-                <nav class="card">
-                    <div class="card-headder mx-auto f">Most Popular</div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><a href="#">Best clothes</a></li>
-                        <li class="list-group-item"><a href="#">Automobiles</a></li>
-                        <li class="list-group-item"><a href="#">Home interior</a></li>
-                        <li class="list-group-item"><a href="#">Electronics</a></li>
-                        <li class="list-group-item"><a href="#">Technologies</a></li>
-                        <li class="list-group-item"><a href="#">Digital goods</a></li>
-                        
-                    </ul>
-                </nav>
-            </aside> <!-- col.// -->
-            <div class="col-md-9 my-auto">
-                <article class="banner-wrap">
-                    <img src="{{asset('img/hero.jpg')}}" class="w-100 rounded" style="height: 350px">
-                </article>
-            </div> <!-- col.// -->
-        </div> <!-- row.// -->
-        </div> <!-- container //  -->
-        </section>
+
+<div class="container-fluid hero-image" style="background-image: linear-gradient(to bottom right,#0997a7,white 85%);">
+    <div class="row">
+        <div class="explore-more fade_effect animated fadeInUpBig col-sm-4 my-auto" style="font-family: URW Chancery L, cursive">
+            <h2>
+                <span>Our Strengths</span>
+                <br><br>
+                <ul>
+                    <li> Center, Off Center &amp; Angle Neck Production Facility. </li>
+                    <br>
+                    <li> 25ml bottle to 5ltr Jerry Can </li>
+                </ul>
+            </h2>
+
+        </div>
+        <div class="col-sm-8 home_image">
+            <img src="{{ asset('img/Home_Page.png')}}" class="w-100" alt="Mixed Bottles Image">
+        </div>
+    </div>
+</div>
+<div class="container-fluid mt-5">
+
+    <div class="row">
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+                        <img src="{{asset("img/grid1.png")}}" alt="Product Image" class="img-thumbnail img-fluid">
+                    <div class="product-desc">
+                        <span class="product-price">
+                            $10
+                        </span>
+                        <small class="text-muted">Category</small>
+                        <a href="#" class="product-name"> Product</a>
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Info <i class="fa fa-long-arrow-right"></i> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+                        <img src="{{asset("img/grid2.png")}}" alt="Product Image" class="img-thumbnail img-fluid">
+                    <div class="product-desc">
+                        <span class="product-price">
+                            $10
+                        </span>
+                        <small class="text-muted">Category</small>
+                        <a href="#" class="product-name"> Product</a>
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Info <i class="fa fa-long-arrow-right"></i> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+                        <img src="{{asset("img/grid3.png")}}" alt="Product Image" class="img-thumbnail img-fluid">
+                    <div class="product-desc">
+                        <span class="product-price">
+                            $10
+                        </span>
+                        <small class="text-muted">Category</small>
+                        <a href="#" class="product-name"> Product</a>
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Info <i class="fa fa-long-arrow-right"></i> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+                        <img src="{{asset("img/grid4.png")}}" alt="Product Image" class="img-thumbnail img-fluid">
+                    <div class="product-desc">
+                        <span class="product-price">
+                            $10
+                        </span>
+                        <small class="text-muted">Category</small>
+                        <a href="#" class="product-name"> Product</a>
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Info <i class="fa fa-long-arrow-right"></i> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,94 +1,94 @@
 
- <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+ <nav class="navbar navbar-expand-md navbar-light shadow-sm font-weight-bold">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                          <a class="nav-link" href="{{url('/')}}">
-                            Home
-                            <span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">
-                            About us
-                          </a>
-                        </li>
-                        <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Product Category
-                          </a>
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            {{-- @foreach ($category as $item)
-                              <a class="dropdown-item" href="{{url('productCategory',['categoryData'=>$item])}}">{{$item['category_name']}}</a>
-                            @endforeach --}}
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                          </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                              Quality
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('brochure')}}">
-                              Brochure
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                              Contact Us
-                            </a>
-                        </li>
-                      </ul>
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @endif
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="{{url('/')}}">
+                    Home
+                    <span class="sr-only">(current)</span>
+                    </a>
+                </li>
 
-                        
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    About us
+                  </a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Product Category
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    {{-- @foreach ($category as $item)
+                      <a class="dropdown-item" href="{{url('productCategory',['categoryData'=>$item])}}">{{$item['category_name']}}</a>
+                    @endforeach --}}
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Quality
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('brochure')}}">
+                      Brochure
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Contact Us
+                    </a>
+                </li>
+              </ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                @if (Route::has('login'))
+                <li class="nav-item">
+                    <a class="nav-link btn bg-white border border-dark px-3 text-dark font-weight-bold" href="{{ route('login') }}">{{ __('Log in') }}</a>
+                </li>
+                @endif
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a href="{{ url('/orders') }}" class="dropdown-item">Oreder Details</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                                {{-- <li> --}}
-                                {{-- </li> --}}
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link px-3 btn border border-dark text-light ml-3 font-weight-bold" style="background-color: #007c89" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+                </li>
+                @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a href="{{ url('/orders') }}" class="dropdown-item">Oreder Details</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('log out') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                        {{-- <li> --}}
+                        {{-- </li> --}}
+                    </div>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
